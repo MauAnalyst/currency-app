cc367b31244e31163df58f6ebd49acbbe4fb7b4c
Mauricio Brito dos Santos <164758301+MauAnalyst@users.noreply.github.com>
2024-12-24 09:03:56 -0300

    feat: add list currency and not found

--- dist/app.js
+++ dist/app.js
@@ -15,25 +15,34 @@ search.addEventListener("submit", (event) => __awaiter(void 0, void 0, void 0, f
     if (searchValue.value.length < 3) {
         return (search.style.borderColor = "red");
     }
-    const coinsList = yield currencyList("aaa");
-    const retorno = Object.keys(coinsList).map((key) => ({
+    const coinsList = yield currencyList();
+    const coinsArray = Object.keys(coinsList).map((key) => ({
         key,
         value: coinsList[key],
     }));
-    console.log(retorno);
-    searchValue.value = "";
+    let check = [];
+    coinsArray.forEach((e) => {
+        if (e.key.toLowerCase() === searchValue.value.toLowerCase() ||
+            e.value.toLowerCase() === searchValue.value.toLowerCase()) {
+            check.push(e);
+        }
+    });
+    if (!check[0]) {
+        return (search.style.borderColor = "red");
+    }
+    searchValue.value = `${check[0].key} - ${check[0].value}`;
+    displayCurrencies(`${check[0].key}-BRL`);
 }));
 document.addEventListener("click", () => {
     search.style.borderColor = "";
 });
-function currencyList(coins) {
+function currencyList() {
     return __awaiter(this, void 0, void 0, function* () {
         const response = yield fetch("https://economia.awesomeapi.com.br/json/available/uniq");
         if (!response.ok) {
             throw new Error("Failed to fetch currency data");
         }
-        const data = response.json();
-        return data;
+        return response.json();
     });
 }
 function currencyData(coins) {
@@ -45,21 +54,48 @@ function currencyData(coins) {
 function displayCurrencies(coin) {
     return __awaiter(this, void 0, void 0, function* () {
         const result = document.querySelector(".result");
+        const currency = document.querySelector("#currency");
         const value = document.querySelector("#currency-in #value");
+        const variation = document.querySelector("#variation");
+        const arrowVariation = document.querySelector("#arrow-variation");
+        const infos = document.querySelector(".infos p");
+        const notfound = document.querySelector(".result-notfound");
+        const textNotFound = document.querySelector("#text-notfound");
         try {
             const data = yield currencyData(coin);
             if (data.status === 200) {
+                result.style.display = "block";
+                notfound.style.display = "none";
                 const dataJson = yield data.json();
                 const dataKey = Object.keys(dataJson)[0];
                 const dataItem = dataJson[dataKey];
+                console.log(dataItem);
                 const price = +dataItem.ask;
-                value.innerHTML = price.toFixed(2).replace(".", ",");
+                const varia = +dataItem.pctChange;
+                if (varia < 0) {
+                    arrowVariation.textContent = "trending_down";
+                    infos.style.backgroundColor = "#de3d3d";
+                }
+                else {
+                    arrowVariation.textContent = "trending_up";
+                    infos.style.backgroundColor = "#05b577";
+                }
+                console.log(dataItem.code);
+                currency.textContent = `1 ${dataItem.code} equivale à`;
+                value.textContent = (+price.toFixed(2)).toLocaleString("pt-br", {
+                    minimumFractionDigits: 2,
+                });
+                variation.textContent = `${varia.toFixed(2).replace(".", ",")}%`;
             }
             else {
+                notfound.style.display = "flex";
+                result.style.display = "none";
+                textNotFound.textContent = `Cotação "${coin}" não encontrada`;
             }
         }
         catch (error) {
+            console.error(error);
+            textNotFound.textContent = `Erro ao buscar cotação: ${error.message}`;
         }
     });
 }
-displayCurrencies("USD-BRL");
--- src/app.ts
+++ src/app.ts
@@ -9,16 +9,31 @@ search.addEventListener("submit", async (event) => {
     return (search.style.borderColor = "red");
   }
 
-  const coinsList = await currencyList("aaa");
+  const coinsList = await currencyList();
 
-  const retorno = Object.keys(coinsList).map((key) => ({
+  const coinsArray = Object.keys(coinsList).map((key) => ({
     key,
     value: coinsList[key],
   }));
 
-  console.log(retorno);
+  let check: { key: string; value: string }[] = [];
 
-  searchValue.value = "";
+  coinsArray.forEach((e) => {
+    if (
+      e.key.toLowerCase() === searchValue.value.toLowerCase() ||
+      e.value.toLowerCase() === searchValue.value.toLowerCase()
+    ) {
+      check.push(e);
+    }
+  });
+
+  if (!check[0]) {
+    return (search.style.borderColor = "red");
+  }
+
+  searchValue.value = `${check[0].key} - ${check[0].value}`;
+
+  displayCurrencies(`${check[0].key}-BRL`);
 });
 
 document.addEventListener("click", () => {
@@ -28,7 +43,7 @@ document.addEventListener("click", () => {
 // --------------- funções
 
 //---- lista de moedas
-async function currencyList(coins: string) {
+async function currencyList() {
   const response = await fetch(
     "https://economia.awesomeapi.com.br/json/available/uniq"
   );
@@ -37,10 +52,7 @@ async function currencyList(coins: string) {
     throw new Error("Failed to fetch currency data");
   }
 
-  const data = response.json();
-
-  // const dataKeys = Object.keys(data)
-  return data;
+  return response.json();
 }
 
 //---- buscando cotação moeda
@@ -54,30 +66,81 @@ async function currencyData(coins: string) {
   return response;
 }
 
+//---- adicionando moeda
 async function displayCurrencies(coin: string) {
-  const result = document.querySelector(".result");
-  const value = document.querySelector("#currency-in #value")!;
+  const result = document.querySelector(".result") as HTMLBodyElement;
+  const currency = document.querySelector("#currency") as HTMLBodyElement;
+  const value = document.querySelector(
+    "#currency-in #value"
+  ) as HTMLBodyElement;
+  const variation = document.querySelector("#variation") as HTMLBodyElement;
+  const arrowVariation = document.querySelector(
+    "#arrow-variation"
+  ) as HTMLBodyElement;
+  const infos = document.querySelector(".infos p") as HTMLBodyElement;
+
+  const notfound = document.querySelector(
+    ".result-notfound"
+  ) as HTMLBodyElement;
+  const textNotFound = document.querySelector(
+    "#text-notfound"
+  ) as HTMLBodyElement;
 
   try {
     const data = await currencyData(coin);
     if (data.status === 200) {
+      result.style.display = "block";
+      notfound.style.display = "none";
       const dataJson = await data.json();
       const dataKey = Object.keys(dataJson)[0];
       const dataItem = dataJson[dataKey];
 
-      const price: number = +dataItem.ask;
+      console.log(dataItem);
 
-      value.innerHTML = price.toFixed(2).replace(".", ",");
+      const price: number = +dataItem.ask;
+      const varia: number = +dataItem.pctChange;
+
+      if (varia < 0) {
+        arrowVariation.textContent = "trending_down";
+        infos.style.backgroundColor = "#de3d3d";
+      } else {
+        arrowVariation.textContent = "trending_up";
+        infos.style.backgroundColor = "#05b577";
+      }
+
+      //   var f = price.toLocaleString("pt-br", {
+      //     style: "currency",
+      //     currency: "BRL",
+      //   });
+      //   console.log(f);
+
+      //   //sem R$
+      //   var f2 = price.toLocaleString("pt-br", { minimumFractionDigits: 2 });
+      //   console.log(f2);
+
+      console.log(dataItem.code);
+
+      currency.textContent = `1 ${dataItem.code} equivale à`;
+
+      value.textContent = (+price.toFixed(2)).toLocaleString("pt-br", {
+        minimumFractionDigits: 2,
+      });
+      variation.textContent = `${varia.toFixed(2).replace(".", ",")}%`;
     } else {
+      notfound.style.display = "flex";
+      result.style.display = "none";
+      textNotFound.textContent = `Cotação "${coin}" não encontrada`;
     }
 
     //   const rates = data.rates;
     //   container.innerHTML = Object.entries(rates)
     //     .map(([currency, rate]) => `<p>${currency}: ${rate.toFixed(2)}</p>`)
     //     .join('');
-  } catch (error) {
+  } catch (error: any) {
+    console.error(error);
+    textNotFound.textContent = `Erro ao buscar cotação: ${error.message}`;
     //   container.innerHTML = `<p>Error: ${error.message}</p>`;
   }
 }
 
-displayCurrencies("USD-BRL");
+// displayCurrencies("USD-BRL");
